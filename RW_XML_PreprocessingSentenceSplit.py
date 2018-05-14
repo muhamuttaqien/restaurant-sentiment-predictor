f9fbc34b3aea0a0b4a17a642c8eefd0d898d07d1
Samuel Yang <samyang811@gmail.com>
2018-05-14 22:38:54 +0700

    added python preprocessing script for sentence splitting only, and other additional python for operate XML elements, Preprocessed data contains less empty sentence

--- RW_XML_PreprocessingSentenceSplit.py
+++ RW_XML_PreprocessingSentenceSplit.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as et
 base_path = os.path.dirname(os.path.realpath(__file__))
 #print(base_path)  print current directory address
 
-xml_file = os.path.join(base_path, "validation_set.xml")
+xml_file = os.path.join(base_path, "training_set.xml")
 #print(xml_file) 
 
 tree = et.parse(xml_file)
@@ -22,11 +22,14 @@ for reviewElement in root:
         #print(subChild.tag,subChild.attrib)
         if subChild.tag == 'text':
            reviewTextContent = subChild.text.split('.')
+           attrNum = 1
            for sentenceNumber in range(0,len(reviewTextContent)):
-               newSentenceEl = et.SubElement(newSentencesEl,'sentence',attrib={"id":str(sentenceNumber)})
-               newSentenceEl.text = reviewTextContent[sentenceNumber].strip()
+               if len(reviewTextContent[sentenceNumber]) != 0:
+                   newSentenceEl = et.SubElement(newSentencesEl,'sentence',attrib={"id":str(attrNum)})
+                   newSentenceEl.text = reviewTextContent[sentenceNumber].strip()
+                   attrNum = attrNum + 1
                #print (reviewTextContent[sentenceNumber].strip())
                #print(newSentenceEl.tag,newSentenceEl.attrib,newSentenceEl.text)
 		
 
-tree.write("validation_set_SentenceSplitted.xml")
+tree.write("training_set_SentenceSplitted.xml")
